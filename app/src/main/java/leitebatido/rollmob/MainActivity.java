eae096845d020d8e8c48813ab4c2d92040a150aa
Weslei Cristo <wesleipcristo@outlook.com>
2016-08-27 16:46:31 -0300

    Thread
    Criando o processo concorrente, para o tempo entre as rolagens.

--- app/src/main/java/leitebatido/rollmob/MainActivity.java
+++ app/src/main/java/leitebatido/rollmob/MainActivity.java
@@ -6,84 +6,70 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private Button botao;
+    private ProgressBar pbTempo;
+    private TextView txvResultado;
+
+    Tempo tempo = new Tempo();
+    Thread thTempo = new Thread(tempo);
+
+    private class Dado {
+        private Dado(int lado){nLados = lado; Rolar();}
+        int nLados;
+        int valor;
+        private void Rolar(){
+            Random gerador = new Random();
+            valor = gerador.nextInt(nLados)+1;
+        }
+        private String Resultado(){
+            return String.valueOf(valor);
+        }
+    }
+
+    private class Tempo implements Runnable{
+        int cont = 0;
+        @Override
+        public void run() {
+            while(!isFinishing()){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (cont > 100){
+                    cont = 0;
+                } else {
+                    cont++;
+                }
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    pbTempo.setProgress(cont);
+                }
+            });
+        }
+        public void setCont(int cont){
+            this.cont = cont;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pbTempo = (ProgressBar)findViewById(R.id.progressBar);
+        txvResultado = (TextView)findViewById(R.id.txtvresultado);
+        thTempo.start();
     }
-
-    ProgressBar barra = (ProgressBar)findViewById(R.id.progressBar);
-    int tempo;
-    contador cont = new contador();
-
-    public class contador extends Thread{
-        @Override
-        public void run() {
-            while(true) {
-                if (tempo >= 100) {
-                    tempo = 0;
-                } else {
-                    tempo++;
-                }
-                barra.setProgress(tempo);
-            }
-        }
-    }
-
-    public class dado {
-        public dado(int lado){nlados = lado;}
-        int nlados;
-        private int rolar(){
-            Random roll = new Random();
-            return roll.nextInt(nlados)+1;
-        }
-    }
-
-    dado d4 = new dado(4);
-    dado d6 = new dado(6);
-    dado d8 = new dado(8);
-    dado d10 = new dado(10);
-    dado d12 = new dado(12);
-    dado d20 = new dado(20);
 
     public void rolar(View view){
-        cont.start();
-        Button dado = (Button)view;
-        switch (Integer.parseInt(dado.getText().toString().substring(1))){
-            case 4: {
-                resultado(d4.rolar());
-                break;
-            }
-            case 6: {
-                resultado(d6.rolar());
-                break;
-            }
-            case 8: {
-                resultado(d8.rolar());
-                break;
-            }
-            case 10: {
-                resultado(d10.rolar());
-                break;
-            }
-            case 12: {
-                resultado(d12.rolar());
-                break;
-            }
-            case 20: {
-                resultado(d20.rolar());
-                break;
-            }
-        }
+        botao = (Button)view;
+        Dado dado = new Dado(Integer.parseInt(botao.getText().toString().substring(1)));
+        txvResultado.setText(dado.Resultado());
     }
-
-    private void resultado(int valor){
-        TextView txt = (TextView)findViewById(R.id.txtvresultado);
-        txt.setText(String.valueOf(valor));
-    }
-
 }
